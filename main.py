2226c225e242c1b7daac6ab68060d5e8260ab371
Cássio <cassiobernardes96@gmail.com>
2021-02-14 14:59:12 -0300

    Tests from third to final

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ class Birthday_candles():
         self.arr = arr
 
 
+    def counting_tallest(self):
+        tallest = 0
+        height = max(self.arr)
+        for i in range(0, len(self.arr)):
+            if self.arr[i] == height:
+                tallest += 1
+        return tallest
--- test_birthday.py
+++ test_birthday.py
@@ -7,6 +7,13 @@ class TestBirthdayCakeCandles(TestCase):
             return
 
     def tests_if_birthday_candles_is_recieving_a_parameter(self):
-        assert result.arr == [1, 2, 3]
+        assert result.arr == [1, 2, 3, 3]
 
-result = Birthday_candles([1, 2, 3])
+    def tests_if_counting_tallest_exists(self):
+        if result.counting_tallest():
+            return
+
+    def tests_counting_tallest(self):
+        assert result.counting_tallest() == 2
+
+result = Birthday_candles([1, 2, 3, 3])
